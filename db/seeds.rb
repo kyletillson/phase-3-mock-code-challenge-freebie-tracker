b4514ce33b040ea5f94fa33f121386a1c02f8215
Kyle Tillson <kyletillson004@gmail.com>
2022-09-08 13:06:42 -0600

    commit tree 82f0e692410d5eaae8fd121bd78a41545fbb01f9

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,10 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name: "Keychain", value: 2)
+Freebie.create(item_name: "Wallet", value: 4)
+Freebie.create(item_name: "Pencil", value: 6)
+Freebie.create(item_name: "Case", value: 8)
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
